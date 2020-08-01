9fbeea363b14dc17d37fdeb531379fb843abdb94
Sawan021 <68999293+Sawan021@users.noreply.github.com>
2020-08-01 17:00:01 +0530

    Update truyum-schema.sql

--- truyum-ui/SQL/truyum-schema.sql
+++ truyum-ui/SQL/truyum-schema.sql
@@ -3,7 +3,7 @@ create database truyumui;
 use truyumui;
 
 create table menu_items(
-	menu_id int primary key,
+    menu_id int primary key,
     menu_name varchar(50) not null,
     menu_price decimal(7,2) not null,
     menu_Active varchar(3) not null,
@@ -13,11 +13,11 @@ create table menu_items(
 );
 
 create table cart(
-	cart_id int primary key,
+    cart_id int primary key,
     user_id int references user(user_id),
     menu_id int references menu_items(menu_id)
 );
 create table user(
-	user_id int primary key,
+    user_id int primary key,
     user_name varchar(50) not null
 );
